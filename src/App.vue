4dbfb6a55f34724f21a4610a89657fe81bd4aa85
Shirley Laurette <Shirleylaurette1@gmail.com>
2023-09-26 09:14:13 +0100

    first comit, header done

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+// import './index.css'
+</script> -->
+
+<template>
+  <Navbar/> 
+
+  <!-- <main>
+    <TheWelcome />
+  </main> -->
+</template>
+<script>
+import TheWelcome from './components/TheWelcome.vue';
+import Navbar from './components/Navbar.vue'
+export default {
+  name:"App",
+  components:{
+    TheWelcome,
+    Navbar,
+  },
+  data() {
+    
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './assets/main.css'
+// import './index.css'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// createApp.config.productionTip = false
+
+
+createApp(App).mount('#app')
